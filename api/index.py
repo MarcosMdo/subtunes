cb19b3c1bba0cc005629261abe34485e49d48fc4
AdrielMendezRios <adriel315@gmail.com>
2023-11-20 17:27:13 -0500

    fixed index.py again

--- api/index.py
+++ api/index.py
@@ -7,6 +7,17 @@ import string
 
 app = Flask(__name__)
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# generate random 16 character string for secrete key, needed for session object
+N = 16
+res = ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+app.config['SECRET_KEY'] = res
+
+from .blueprints.spotify_auth_api import bp as spotify_auth_api
+app.register_blueprint(spotify_auth_api)
+
+from .blueprints.tunes_api import bp as tunes_api
+app.register_blueprint(tunes_api)
+
+@app.route("/")
+def index():
+    return redirect(url_for('spotify_auth_api.login'))
