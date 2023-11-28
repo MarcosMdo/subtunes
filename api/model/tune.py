3c5746539a54e1d1dd046ebf0ef922cd4513b785
AdrielMendezRios <adriel315@gmail.com>
2023-11-28 11:08:54 -0500

    add tunemodel, save model to db when ep hit

--- api/model/tune.py
+++ api/model/tune.py
@@ -1,12 +1,20 @@
 from dataclasses import dataclass
+from ..database.db import db
 
-@dataclass(init=True, repr=True, eq=True, order=True, frozen=True)
-class TuneModel():
-    id : int
-    url : str
-    uri : str
-    name : str
-    artist : str
-    album : str
-    image_url : str
-    duration : int
+@dataclass(init=True, repr=True, eq=True, order=True)
+class TuneModel(db.Model):
+    id = db.Column(db.String(100), primary_key=True)
+    url  = db.Column(db.String(100), unique=True, nullable=False)
+    uri  = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    artist = db.Column(db.String(100), nullable=False)
+    album  = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(100))
+    duration = db.Column(db.Integer, nullable=False)
+    
+    
+    def __repr__(self):
+        return f"<Tune {self.name} by {self.artist}>"
+    
+    def __str__(self):
+        return f"{self.name} by {self.artist}"
