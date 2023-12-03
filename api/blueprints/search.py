fbebeac94447983a7e83c5b6fdf57ec27884811e
Marcos Montes de Oca <26945566+MarcosMdo@users.noreply.github.com>
2023-12-03 13:27:58 -0800

    Adds Spotify search functionality for a tune (#5)
    * adds basic search page and search functionality
    
    * adds login to home page, displays song info on search page after searching
    
    * adds id to search object
    
    * removes unused endpoint
    
    ---------
    
    Co-authored-by: Marcos Montes de Oca <marcos@Marcoss-MacBook-Pro-2.local>

---
+++ api/blueprints/search.py
@@ -0,0 +1,58 @@
+"""" This module contains the blueprint for the tunes api endpoints. """
+
+from urllib.parse import quote
+from flask import Flask, request, redirect, session, url_for, Blueprint, jsonify, current_app
+from ..database.db import db
+from ..model.tune import Tune
+from ..spotify_api_endpoints import spotify_endpoints
+from ..blueprints.spotify_auth_api import get_auth_header
+import requests
+
+
+bp = Blueprint('search_api', __name__)
+
+SPOTIFY_API_URL = spotify_endpoints['SPOTIFY_API_URL']
+SPOTIFY_SEARCH_API_URL = f"{SPOTIFY_API_URL}/search"
+
+the_query="nadie&type=album&include_external=audio&locale=en-US%2Cen%3Bq%3D0.9%2Ces%3Bq%3D0.8&offset=0&limit=20"
+
+@bp.route("/api/search/tune", methods=["GET"])
+def search_tune(query = "name=nadie sabe lo que"):
+    # Get the search term from the query parameters
+    query = "name={}".format(request.args.get('query'))
+    
+    current_app.logger.info(query)
+
+    if not query:
+        return jsonify({'error': 'Query parameter "query" is required'}), 400
+
+
+    auth_header = get_auth_header(session['expire_time'])
+    params = {
+        "q": query,
+        "type": "track,playlist,artist,album",
+        "limit": 10,
+        "offset": 0,
+        "include_external": "audio"
+    }
+
+    response = requests.get(SPOTIFY_SEARCH_API_URL, headers=auth_header, params=params)
+
+    current_app.logger.info(response)
+
+    if response.status_code == 200:
+        # Parse the response and extract track information
+        tracks = response.json().get('tracks', {}).get('items', [])
+
+        # Extract relevant information from each track
+        track_info = [{'id': track['id'], 'name': track['name'], 'artist': track['artists'][0]['name'], 'external': track['preview_url']} for track in tracks]
+
+        return jsonify({'tracks': track_info})
+    else:
+        return jsonify({'error': 'Failed to fetch tracks from Spotify'}), response.status_code
+
+@bp.route("/search/subtune/<name>")
+def search_subtune(query = ""):
+    pass
+
+
---
+++ app/search/page.tsx
@@ -0,0 +1,55 @@
+// pages/index.js
+'use client'
+
+import { useState, useEffect } from 'react';
+import '../globals.css';
+import './page.css'
+
+const Home = () => {
+  const [query, setQuery] = useState('');
+  const [musicData, setMusicData] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`/api/search/tune?query=${encodeURIComponent(query)}`);
+      const data = await response.json();
+
+      // Update the state with the fetched data
+      setMusicData(data['tracks']);
+      console.log(data)
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    // You can add additional logic or use fetchData() if needed
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Enter your search query"
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+
+      <ul>
+        {musicData.map((item, index) => (
+          <li key={index}>
+            <p>ID: {item.id}</p>
+            <p>Name: {item.name}</p>
+            <p>Artist: {item.artist}</p>
+            <a href={item.external}>preview song</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
