1a5807b4b7af0985f4180c64ae57ad7ef7c4ab09
AdrielMendezRios <adriel315@gmail.com>
2023-12-21 18:33:00 -0500

    refactored subtune & tunes endpoints

--- api/blueprints/subtunes_api.py
+++ api/blueprints/subtunes_api.py
@@ -3,9 +3,9 @@ import copy
 import uuid
 from urllib.parse import quote
 
-
 from ..database.db import db
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import insert
 
 from ..model.subtune import Subtune
 from ..model.subtune_tune import Subtune_Tune
@@ -24,6 +24,7 @@ bp = Blueprint('subtunes_api', __name__)
 
 SPOTIFY_API_URL = spotify_endpoints['SPOTIFY_API_URL']
 
+# get subtune by id
 @bp.route("/subtune/<id>", methods=["GET"])
 @login_required
 def get_subtune_by_id(id=1):
@@ -41,16 +42,19 @@ def get_subtune_by_id(id=1):
         if subtune.user_id != user_id:
             return {"status": "user does not own this subtune"}, 401
         
-        # get all records in subtune_tunes table for this subtune
-        subtune_tunes = sorted(subtune.subtune_tunes, key=lambda subtune_tune: subtune_tune.order_in_subtune)
-        # get all the tune records in tune table for this subtune given the subtune_tunes link table
-        tune_objects = [subtune_tune.tune for subtune_tune in subtune_tunes]
-        tunes_in_subtune[subtune.id] = [tune for tune in tune_objects]
+        response = {"title": subtune.name, "description": subtune.description}
         
-        return tunes_in_subtune, 200
+        # get relavent rows from link table
+        subtune_tunes = sorted(subtune.subtune_tunes, key=lambda subtune_tune: subtune_tune.order_in_subtune)
+        
+        # get tunes from link table 
+        response["tunes"] = [subtune_tune.tune for subtune_tune in subtune_tunes]
+        
+        return response, 200
         
     return {"Error": "something went really wrong"}, 500
 
+# get all subtunes for a user
 @bp.route("/user/<user_id>/subtunes", methods=["GET"])
 @login_required
 def get_user_subtunes(user_id=-1):
@@ -60,37 +64,41 @@ def get_user_subtunes(user_id=-1):
 
         if not user_subtunes:
             return {"status": "no subtunes found for this user"}, 404
-        subtune_res = {}
-        # should we enforce unique subtune names??
+        
+        response = []
+
         for subtune in user_subtunes:
             res, code = get_subtune_by_id(subtune.id)
-            current_app.logger.info(f"res: {res}")
+
             if "status" in res or "Error" in res:
                 return res, code
-            subtune_res[subtune.name] = res
-        return subtune_res, 200
+            
+            response.append(res)
+        
+        return response, 200
 
-# dummy_data = ["5YY7ht3PCArlLjLbcTiAvh", "4urciuKll77Us0CpoAaYt0", "2zSuWrakRZKwWaLtHUvtnz", "7v8kE3NhsgbTtJnI0fwoss", "2Nd1dImwW0VVN5HJ9MfvUd"]
-dummy_data = ["1jlKdNbOA90rjnt88GJnwO", "1xuYajTJZh8zZrPRmUaagf", "3kNVYo6BJE9AENxzokM9YC", "1q8NdCAQ9QUjpYiqzdd3mv", "1U5X9bPu8xXHuejzUAi4Bx"]
-
-@bp.route("/create/subtune", methods=["POST", "GET"]) # `/create/` & `GET` for testing for now, should remove once we have a front end
+# save subtune to db
+@bp.route("/subtune", methods=["POST"])
 @login_required
 def save_subtune():
     """
     NOTE: added a `error_with_tunes` field to the response, this will be a list of 
     errors if any for a given tune id. may not need but just in case for now.
     """
-    # maybe we want to pass data through body instead of query params once front end is set up
-    subtune_name = request.args.get('name') if request.args.get('name') \
-        else f"{current_user.display_name}'s subtune {uuid.uuid4().hex[:6]}"
-    subtune_desc = request.args.get('description') if request.args.get('description') else "test description"
     
-    tunes_arg = request.args.getlist('tunes')
-    tune_ids = tunes_arg.split(",") if tunes_arg else dummy_data # change to [] eventually
-    if len(tune_ids) == 0:
+    request_body = request.get_json()
+    
+    if "name" not in request_body:
+        return {"status": "subtune name is required"}, 400
+    if "tunes" not in request_body or len(request_body["tunes"]) == 0:
         return {"status": "no tunes given"}, 400
+    
+    
+    name = request_body["name"]
+    description = request_body["description"]
+    tune_ids = request_body["tunes"]
 
-    subtune= Subtune(name=subtune_name, description=subtune_desc, user_id=current_user.id)
+    subtune= Subtune(name=name, description=description, user_id=current_user.id)
     db.session.add(subtune)
     
     error_with_tunes = []
@@ -107,19 +115,62 @@ def save_subtune():
         # 'add' tune to subtune
         subtune.subtune_tunes.append(Subtune_Tune(tune_id=tune_id, order_in_subtune=idx))
 
-    # save all subtune_tune's and subtune object to db
+    # save all subtune_tune's and the subtune object to db
     db.session.commit()
     
     error_with_tunes = error_with_tunes if len(error_with_tunes) > 0 else None
     return {"subtune": subtune, "Error with tunes": error_with_tunes}, 200
 
-@bp.route("/delete/subtune/<id>", methods=["DELETE", 'GET']) # `/delete/` & `GET` added just for testing for now, should remove once we have a front end
+# update subtune
+@bp.route("/subtune/<id>", methods=["PUT"])
+def update_subtune(id=-1):
+    subtune = Subtune.query.get(id)
+    
+    if subtune is None:
+        return {"status": "subtune not found"}, 404
+    
+    body = request.get_json()
+    
+    if "name" in body:
+        subtune.name = body["name"]
+    if "description" in body:
+        subtune.description = body["description"]
+    
+    if "new_tunes" in body:
+        new_tunes = body["new_tunes"]
+        
+        for tune_id in new_tunes:
+            res, http_res_code = get_tune(tune_id)
+            
+            # something went wrong retrieving the tune
+            if "tune" not in res:
+                return {"Error": res, "HTTPResponse code": http_res_code}, http_res_code
+            
+            # 'add' tune to subtune
+            stmt = insert(Subtune_Tune).values(subtune_id=1, tune_id=tune_id)
+            stmt = stmt.on_conflict_do_nothing(index_elements=['subtune_id', 'tune_id'])
+            db.session.execute(stmt)
+    
+    #deletes tune from subtune, or do nothing
+    if "remove_tunes" in body:
+        for tune_id in body["remove_tunes"]:
+            subtune_tune = Subtune_Tune.query.filter_by(subtune_id=id, tune_id=tune_id).delete()
+    
+    if "order" in body:
+        for idx, tune_id in enumerate(body["order"]):
+            subtune_tune = Subtune_Tune.query.filter_by(subtune_id=id, tune_id=tune_id).first()
+            subtune_tune.order_in_subtune = idx
+    
+    db.session.commit()
+    return {"subtune": subtune}, 200
+    
+
+
+
+# Delete subtune from db
+@bp.route("/subtune/<id>", methods=["DELETE"]) 
+@login_required
 def delete_subtune(id=1):
-    """
-        For now this endpoint deletes any subtune with the given id, no user authentication is done.
-        The same action can be done with pgadmin, but maybe we want a similar endpoint in the future
-        and another default endpoint that requires a user to be logged in to use in the front end
-    """
     with current_app.app_context():
         subtune = Subtune.query.get(id)
         if subtune is None:
@@ -127,5 +178,3 @@ def delete_subtune(id=1):
         db.session.delete(subtune)
         db.session.commit()
         return {"status": "subtune deleted"}, 200
-
-
--- api/model/subtune.py
+++ api/model/subtune.py
@@ -6,7 +6,7 @@ class Subtune(db.Model):
     __tabename__ = 'subtune'
     id: int = db.Column(db.Integer, primary_key=True)
     name: str = db.Column(db.String(100), nullable=False)
-    description: str = db.Column(db.String(150), nullable=False)
+    description: str = db.Column(db.String(150), nullable=True)
     user_id: int = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
     subtune_tunes = db.relationship('Subtune_Tune', backref='Subtune', lazy='subquery', cascade="all, delete-orphan")
