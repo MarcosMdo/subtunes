2749c1650084abc1883cb8b57dcaa2204790c2da
Marcos Montes de Oca <26945566+MarcosMdo@users.noreply.github.com>
2023-12-28 17:35:46 -0800

    adds things

--- app/boilerplate/components/Subtune.tsx
+++ app/boilerplate/components/Subtune.tsx
@@ -12,8 +12,7 @@ type Subtune = {
   tunes: Array<TuneObj>;
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 const Subtune = ({ subtuneObj }) => {
   console.log(subtuneObj);
   const subtune: Subtune = subtuneObj;
@@ -26,44 +25,6 @@ const Subtune = ({ subtuneObj }) => {
       </div>
       <div className='tunes'>
         <TuneList tunes={subtune.tunes} onAddTune={null}/>
-=======
-const Subtune = ({ subtune }) => {
-  const id = subtune["id"];
-  const name = subtune["name"];
-  const description = subtune["description"];
-  const tunes = subtune["tunes"];
-=======
-const Subtune = ({ subtuneObj }) => {
-  console.log(subtuneObj);
-  const subtune: Subtune = subtuneObj;
-  console.log("\n\n\n\n");
-  console.log("subtune: " + subtune);
-  // const id = subtune["id"];
-  // const name = subtune["name"];
-  // const description = subtune["description"];
-  // const tunes = subtune["tunes"];
->>>>>>> b767d8d (adds viewsubtune and viewsubtunes view which pulls from backend)
-
-  // until endpoint on backend returns these fields, need to blank them out.
-  //const album = tuneData["album"]["name"];
-  //const imageUrl = tuneData["album"]["images"][0]["url"];
-  //const duration = tuneData["duration_ms"];
-  //const popularity = tuneData["popularity"];
-  //const uri = tuneData["uri"];
-
-  return (
-    <div>
-      <div className='subtune-meta'>
-        <p>Title: {subtune.name}</p>
-        <p>Description: {subtune.description}</p>
-      </div>
-      <div className='tunes'>
-<<<<<<< HEAD
-        <TuneList tunes={tunes} onAddTune={null}/>
->>>>>>> 52b6049 (intermediary commit)
-=======
-        <TuneList tunes={subtune.tunes} onAddTune={null}/>
->>>>>>> b767d8d (adds viewsubtune and viewsubtunes view which pulls from backend)
       </div>
     </div>
   );
